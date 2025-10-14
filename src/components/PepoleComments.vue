0f8ac44eefa2f877738bee30cc9cfdb7374a511f
unknown <hatef.dev21@gmail.com>
2025-10-14 20:48:28 +0330

    add comments

--- src/components/PepoleComments.vue
+++ src/components/PepoleComments.vue
@@ -16,34 +16,82 @@
     </div>
 
     <div class="text-white mt-10">
-      <swiper
-        class="border-y border-white/20"
-        :slides-per-view="1.5"
-        :space-between="50"
-        :centered-slides="false"
-        :loop="true"
-        :breakpoints="{
-          640: { slidesPerView: 1.2, spaceBetween: 20 },
-          768: { slidesPerView: 1.5, spaceBetween: 30 },
-          1024: { slidesPerView: 1.7, spaceBetween: 50 },
-        }"
-        :modules="[Navigation]"
-        navigation
-      >
-        <swiper-slide v-for="comment in comments" :key="comment.id">
-          <div class="flex flex-col gap-y-10">
-            <p class="text-5xl font-medium">"{{ comment.comment }}"</p>
+      <div class="relative">
+        <swiper
+          class="border-y border-white/20"
+          :slides-per-view="1.5"
+          :space-between="50"
+          :centered-slides="false"
+          :loop="true"
+          :breakpoints="{
+            640: { slidesPerView: 1.2, spaceBetween: 20 },
+            768: { slidesPerView: 1.5, spaceBetween: 30 },
+            1024: { slidesPerView: 1.7, spaceBetween: 50 },
+          }"
+          :modules="[Navigation]"
+          :navigation="{
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          }"
+        >
+          <swiper-slide v-for="comment in comments" :key="comment.id">
+            <div class="flex flex-col gap-y-10">
+              <p class="text-5xl font-medium">"{{ comment.comment }}"</p>
 
-            <div class="flex items-center gap-x-4">
-              <img :src="comment.image" class="w-12 h-12 rounded-full" />
-              <div class="flex flex-col">
-                <span class="text-sm font-medium">{{ comment.name }}</span>
-                <span class="text-xs text-gray-400">{{ comment.work }}</span>
+              <div class="flex items-center gap-x-4">
+                <img :src="comment.image" class="w-12 h-12 rounded-full" />
+                <div class="flex flex-col">
+                  <span class="text-sm font-medium">{{ comment.name }}</span>
+                  <span class="text-xs text-gray-400">{{ comment.work }}</span>
+                </div>
               </div>
             </div>
+          </swiper-slide>
+        </swiper>
+
+        <!-- Navigation buttons inside the same container -->
+        <div class="flex">
+          <div
+            class="swiper-button-prev-next shrink-0 border px-8 w-[160px] flex justify-between items-center gap-x-5 py-4 border-white/40"
+          >
+            <div
+              class="swiper-button-prev w-10 h-10 flex items-center justify-center cursor-pointer"
+            >
+              <svg
+                width="24"
+                height="24"
+                class="-rotate-90"
+                data-name="1-Arrow Up"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 32 32"
+              >
+                <path
+                  fill="currentColor"
+                  d="m26.71 10.29-10-10a1 1 0 0 0-1.41 0l-10 10 1.41 1.41L15 3.41V32h2V3.41l8.29 8.29z"
+                />
+              </svg>
+            </div>
+            <div class="w-[0.5px] h-full bg-white/40"></div>
+            <div
+              class="swiper-button-next w-10 h-10 flex items-center justify-center cursor-pointer"
+            >
+              <svg
+                width="24"
+                height="24"
+                class="rotate-90"
+                data-name="1-Arrow Up"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 32 32"
+              >
+                <path
+                  fill="currentColor"
+                  d="m26.71 10.29-10-10a1 1 0 0 0-1.41 0l-10 10 1.41 1.41L15 3.41V32h2V3.41l8.29 8.29z"
+                />
+              </svg>
+            </div>
           </div>
-        </swiper-slide>
-      </swiper>
+        </div>
+      </div>
     </div>
   </div>
 </template>
