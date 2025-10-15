52c4885437219e73a25dd90f11c6ebae73cb99a7
unknown <hatef.dev21@gmail.com>
2025-10-15 18:55:44 +0330

    add AboutPage

---
+++ src/components/AboutPage/AboutPageHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="my-container">
+    <div class="grid grid-cols-1 text-white lg:grid-cols-2 gap-y-8">
+      <transition-animation>
+        <h1 class="text-6xl lg:text-8xl uppercase font-semibold">About Our Company</h1>
+      </transition-animation>
+      <div class="flex flex-col gap-y-8">
+        <transition-animation>
+          <p class="text-xl text-white/70 font-medium">
+            We are a dynamic team of digital innovators, strategists, and creatives who are
+            passionate about driving growth for businesses through innovative digital solutions. we
+            blend strategy, creativity.
+          </p>
+        </transition-animation>
+        <transition-animation :delay="0.5">
+          <the-button :backgroundSecondary="true" path="/contact">
+            <template #label> Book a Call </template>
+          </the-button>
+        </transition-animation>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import TransitionAnimation from '../Ui/TransitionAnimation.vue'
+export default {
+  components: { TransitionAnimation },
+  name: 'AboutPageHeader',
+}
+</script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,28 @@
 <template>
-  <h1>About</h1>
+  <the-header></the-header>
+  <div class="mt-40 lg:mt-56">
+    <about-page-header></about-page-header>
+    <div class="my-container mt-16 flex flex-col gap-y-36 justify-center items-center">
+      <transition-animation>
+        <img src="/AboutPageImage.webp" alt="" class="w-full h-auto rounded-2xl" />
+      </transition-animation>
+      <transition-animation>
+        <p class="text-white font-semibold text-4xl text-center w-[825px]">
+          Empowering businesses with data-driven, user-focused strategies that elevate their online
+          presence and transform their digital landscape
+        </p>
+      </transition-animation>
+    </div>
+  </div>
 </template>
+<script>
+import AboutPageHeader from '@/components/AboutPage/AboutPageHeader.vue'
+import TransitionAnimation from '@/components/Ui/TransitionAnimation.vue'
+export default {
+  name: 'AboutView',
+  components: {
+    AboutPageHeader,
+    TransitionAnimation,
+  },
+}
+</script>
